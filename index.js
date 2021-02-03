c24c9b0e66f82f0778ba1454f74223a5d1db639f
Raveen K Sharma <raveen08@gmail.com>
2021-02-03 20:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 // Write your code 
+<<<<<<< HEAD
 
 let random = Math.floor(Math.random() * 10) + 1;
 
@@ -16,3 +17,21 @@ const multiply = num1 * num2;
 const mod = num3 % num4;
 
 const max = Math.max(6, 7, 20);
+=======
+const mod = num3 % num4;
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num1 = 31;
+
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+num3 = 16; 
+
+num4 = 6
+
+
+
+let max = Math.max(6, 7, 20);
+>>>>>>> 4927a3f68c0998c49e46debcd486e9008a18e629
